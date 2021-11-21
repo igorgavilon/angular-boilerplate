a94ce5b036ffd843d8cf6b32b2ad5b34d92abf45
igorgavilon <igorgavilon@gmail.com>
2021-11-20 21:16:20 -0400

    melhoria, utilizando o localstorage como base de dados

--- src/app/@core/services/cliente.service.ts
+++ src/app/@core/services/cliente.service.ts
@@ -1,10 +1,9 @@
 import { Injectable } from "@angular/core";
 import { Router } from "@angular/router";
 import ICliente from "../common/interfaces/cliente.interface";
-import { EnumStatusCliente } from "../common/tipos/tipos";
 
 /**
- * classe de serviço que fará todo acesso à base de dados de clientes
+ * classe de serviço que fará todo acesso à base de dados de clientes que, no caso, é o localstorage
  */
 @Injectable({
     providedIn: 'root'
@@ -18,7 +17,14 @@ export class ClienteService {
      * @returns um array de objetos do tipo ICliente
      */
     public retornaTodosClientes(): ICliente[] {
-        return CLIENTES;
+        //realiza a leitura dos dados do localstorage
+        const dadosLocalStorage: string | null = localStorage.getItem('clientesArray');
+        let arrayClientes: ICliente[] = [];
+        //se já houver algum dado no localstorage, salva no array que será retornado
+        if(dadosLocalStorage !== null) {
+            arrayClientes = JSON.parse(dadosLocalStorage);
+        }
+        return arrayClientes;
     }
 
     /**
@@ -27,30 +33,42 @@ export class ClienteService {
      * @returns um objeto do tipo ICliente
      */
     public buscaClientePeloId(id: number): ICliente {
-        return CLIENTES.find((cliente: ICliente) => cliente.id === id);
+        const arrayClientes: ICliente[]= this.retornaTodosClientes();
+        return arrayClientes.find((cliente: ICliente) => cliente.id === id);
     }
 
     /**
      * função que salva os dados do cliente (novo ou jpá existente) na base de dados
      */
     public salvar(cliente: ICliente): void {
+        const arrayClientes: ICliente[]= this.retornaTodosClientes();
+
         //se o cliente já existe, possui um id
         //serão atualizados os dados de um cliente já existente
-        if(cliente.id) {
-            const indice = CLIENTES.findIndex((clienteAtual: ICliente) => clienteAtual.id === cliente.id);
-            CLIENTES[indice] = cliente;
+        if(cliente.id !== null) {
+            const indice = arrayClientes.findIndex((clienteAtual: ICliente) => clienteAtual.id === cliente.id);
+            arrayClientes[indice] = cliente;
         }else {//se o cliente não possui um id, ele é um novo cliente
             //serão salvos os dados do novo cliente
 
-            //busca o último id que foi cadastrado
-            const ultimoId: number = CLIENTES.map((item: ICliente) => item.id).reduce((anterior: number, atual: number) => Math.max(anterior, atual));
-            //incrementa o último id para definir o id do cliente que será criado
-            const idAtual: number = ultimoId+1;
+            //se for o primeiro cliente a ser cadastrado seu índice será zero
+            let idAtual: number = 0;
+            //se não for o primeiro cliente o arrayClientes não será vazio
+            //então atualiza o idAtual
+            if(arrayClientes.length !== 0) {
+                //busca o último id que foi cadastrado
+                const ultimoId: number = arrayClientes.map((item: ICliente) => item.id).reduce((anterior: number, atual: number) => Math.max(anterior, atual));
+                //incrementa o último id para definir o id do cliente que será criado
+                idAtual = ultimoId+1;
+            }
+
             //guarda os dados do cliente que vieram do formulário
             const {nome, email, telefone, status} = cliente;
             //adiciona o novo cliente no array de clientes
-            CLIENTES.push({id: idAtual, nome: nome, email: email, telefone: telefone, status: status});
+            arrayClientes.push({id: idAtual, nome: nome, email: email, telefone: telefone, status: status});
         }
+
+        localStorage.setItem('clientesArray', JSON.stringify(arrayClientes));
 
         //volta para a tela de listagem do cliente
         this.irRotaListagemCliente();
@@ -61,6 +79,8 @@ export class ClienteService {
      * @param id id do cliente a ser excluído
      */
     public excluirClientePeloId(id: number): void {
+        let arrayClientes: ICliente[]= this.retornaTodosClientes();
+
         //busca o cliente que deve ser excluído
         //salva na variável , caso exista
         const clienteASerDeletado: ICliente = this.buscaClientePeloId(id);
@@ -68,7 +88,8 @@ export class ClienteService {
         //caso exista o cliente que foi buscado no passo anterior
         //filtra o array de clientes mantendo somente os que têm o id diferente do passado para essa função
         if(clienteASerDeletado) {
-            CLIENTES = CLIENTES.filter((clienteAtual: ICliente) => clienteAtual.id !== id);
+            arrayClientes = arrayClientes.filter((clienteAtual: ICliente) => clienteAtual.id !== id);
+            localStorage.setItem('clientesArray', JSON.stringify(arrayClientes));
         }
     }
 
@@ -79,22 +100,3 @@ export class ClienteService {
         this.rota.navigate(['/clientes/listagem']);
     }
 }
-
-//array do tipo ICliente
-//dados mock
-var CLIENTES: ICliente[] = [
-    {
-        id: 1,
-        nome: 'André',
-        email: 'andre@gmail.com',
-        telefone: '67 99990000',
-        status: EnumStatusCliente.ATIVO
-    },
-    {
-        id: 2,
-        nome: 'Beatriz',
-        email: 'beatriz@gmail.com',
-        telefone: '67 99991111',
-        status: EnumStatusCliente.INATIVO
-    }
-];
